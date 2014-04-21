a3fb068d1aec1b39d7860ecebafc51f447404014
Michael Fiano <michael.fiano@gmail.com>
2014-04-21 14:10:57 -0400

    Small fix to documentation and forgot to add the new file.

---
+++ database-trades.lisp
@@ -0,0 +1,47 @@
+(in-package :procrypt)
+
+(defclass trades ()
+  ((id :accessor id
+       :initarg :id
+       :col-type serial)
+   (timestamp :accessor timestamp
+              :initarg :timestamp
+              :col-type bigint)
+   (user-id :accessor user-id
+            :initarg :user-id
+            :col-type integer)
+   (action :accessor action
+           :initarg :action
+           :col-type text)
+   (base-coin :accessor base-coin
+              :initarg :base-coin
+              :col-type text)
+   (quote-coin :accessor quote-coin
+               :initarg :quote-coin
+               :col-type text)
+   (price :accessor price
+          :initarg :price
+          :col-type bigint)
+   (quantity :accessor quantity
+             :initarg :quantity
+             :col-type double-precision))
+  (:keys id)
+  (:metaclass dao-class))
+
+(deftable trades
+  (!dao-def)
+  (!foreign 'users 'user-id 'id)
+  (!foreign 'coins 'base-coin 'code)
+  (!foreign 'coins 'quote-coin 'code))
+
+(defun insert-trade (order-id)
+  "Inserts an order record into the trades table, and sets its timestamp
+   to the current time."
+  (with-connection *db*
+    (unless (get-dao 'trades order-id)
+      (query (:insert-into 'trades
+              (:select '* :from 'orders
+               :where (:= 'id order-id))))
+      (let ((trade-row (get-dao 'trades order-id)))
+        (setf (timestamp trade-row) (get-universal-time))
+        (update-dao trade-row)))))
